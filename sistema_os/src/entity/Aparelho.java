09565818619e52c3b8cb485b87f4658cbf2e4eae
AllanPostai <allan-mpostai@estudante.rs.gov.br>
2025-04-02 22:12:38 -0300

    Atualizando implementação de Aparelho

--- sistema_os/src/entity/Aparelho.java
+++ sistema_os/src/entity/Aparelho.java
@@ -1,6 +1,54 @@
-
 package entity;
 
 public class Aparelho {
-    
+
+    private int id;
+    private String marca;
+    private String modelo;
+    private String descricao;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Aparelho(int id, String marca, String modelo, String descricao) {
+        this.id = id;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.descricao = descricao;
+    }
+
+    public Aparelho() {
+    }
+
+
+
 }
--- sistema_os/src/repository/AparelhoRepository.java
+++ sistema_os/src/repository/AparelhoRepository.java
@@ -1,7 +1,38 @@
 package repository;
 
-public class AparelhoRepository {
-    
-   
+import entity.Aparelho;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import resources.UtilDb;
 
+public class AparelhoRepository {
+
+    private final UtilDb util = new UtilDb();
+    Connection conn;
+    PreparedStatement ppst;
+
+    public Aparelho salvarAparelho(Aparelho aparelho) {
+        conn = util.conexao();
+        String sql = "INSERT INTO aparelho("
+                + "descricao, "
+                + "marca, "
+                + "modelo ) "
+                + "VALUES(?,?,?)";
+
+        try {
+            ppst = conn.prepareStatement(sql);
+            ppst.setString(1, aparelho.getDescricao());
+            ppst.setString(2, aparelho.getMarca());
+            ppst.setString(3, aparelho.getModelo());
+
+            ppst.executeUpdate();
+            ppst.close();
+            conn.close();
+            return aparelho;
+        } catch (SQLException ex) {
+            System.out.println(ex);
+            return null;
+        }
+    }
 }
