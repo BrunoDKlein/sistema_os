ea8d17ee0772405ab91e6ed1c683a72573a404c6
victorkreuz <victor-mmkreuz@educar.rs.gov.br>
2025-04-02 21:25:24 -0300

    Atualizando a implementação de pagamentos

--- sistema_os/src/entity/Pagamento.java
+++ sistema_os/src/entity/Pagamento.java
@@ -1,8 +1,48 @@
-
 package entity;
 
 // * @author Victor
- 
+import java.util.List;
+
 public class Pagamento {
-    
+
+    private int id;
+    private String nomeDoPagador;
+    private int numeroNota;
+    private String data;
+    private int valor;
+    private String metodoPagamento;
+
+    public Pagamento(int id, String nomeDoPagador, int numeroNota, String data, int valor, String metodoPagamento) {
+        this.id = id;
+        this.nomeDoPagador = nomeDoPagador;
+        this.numeroNota = numeroNota;
+        this.data = data;
+        this.valor = valor;
+        this.metodoPagamento = metodoPagamento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nomeDoPagador;
+    }
+
+    public int getNumeroPaginas() {
+        return numeroNota;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public int getvalor() {
+        return valor;
+    }
+
+    public String getMetodoPagamento() {
+        return metodoPagamento;
+    }
+
 }
--- sistema_os/src/repository/PagamentoRepository.java
+++ sistema_os/src/repository/PagamentoRepository.java
@@ -1,8 +1,51 @@
-
 package repository;
 
 // * @author Victor
- 
+
+import entity.Pagamento;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import resources.UtilDb;
+
 public class PagamentoRepository {
-    
+
+    PagamentoRepository pagamentoRepository = new PagamentoRepository();
+
+    private final UtilDb util = new UtilDb();
+    Connection conn;
+    PreparedStatement ppst;
+
+    public Pagamento salvarPagamento(Pagamento pagamento) {
+        conn = util.conexao();
+        String sql = "INSERT INTO pagamento("
+                + "id,"
+                + "id_os,"
+                + "data_pagamento,"
+                + "valor,"
+                + "metodo) "
+                + "VALUES(?,?,?,?,?)";
+
+        try {
+
+            ppst = conn.prepareStatement(sql);
+            ppst.setInt(1, pagamento.getId());
+            ppst.setInt(2, pagamento.getOs());
+            ppst.setString(3, pagamento.getData());
+            ppst.setInt(4, pagamento.getValor());
+            ppst.setString(5, pagamento.getMetodoPagamento());
+
+            ppst.executeUpdate();
+            ppst.close();
+            conn.close();
+            return pagamento;
+
+        } catch (SQLException ex) {
+
+            System.out.println(ex);
+            return null;
+
+        }
+    }
+
 }
